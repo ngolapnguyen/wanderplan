07a48b4a9c65613bada97fbc2d85c2554f382747
hang nhung <hangbichnhung@gmail.com>
2024-10-30 16:44:50 +0700

    feat(map):  test

--- src/features/map/components/map/Map.tsx
+++ src/features/map/components/map/Map.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Map: React.FC = () => {
   return (
     <div className='w-full h-full border rounded-xl flex items-center justify-center bg-white'>
-      <p className='text-xl font-bold'>Put map here</p>
+      <p className='text-xl font-bold'>Test</p>
     </div>
   );
 };
