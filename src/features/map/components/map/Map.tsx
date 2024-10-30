540196d688840007fe045a9cfadc44f7365a46ef
hang nhung <hangbichnhung@gmail.com>
2024-10-30 17:19:28 +0700

    feat(map): add mapbox gl

--- src/features/map/components/map/Map.tsx
+++ src/features/map/components/map/Map.tsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import {
+  useEffect,
+  useRef,
+  //useState
+} from 'react';
+// import { SearchBox } from '@mapbox/search-js-react';
+import mapboxgl from 'mapbox-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export const Map: React.FC = () => {
+  const mapContainerRef = useRef(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  // const [mapLoaded, setMapLoaded] = useState(false);
+  // const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (mapRef.current) return;
+    mapboxgl.accessToken = import.meta.env.VITE_API_KEY;
+
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current!,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [105.8342, 21.0278],
+      zoom: 12,
+    });
+
+    // Add navigation controls to the map
+    mapRef.current.addControl(new mapboxgl.NavigationControl());
+
+    mapRef.current.on('load', () => {
+      // setMapLoaded(true);
+      // mapRef.current.addSource('route', {
+      //   type: 'geojson',
+      //   data: {
+      //     type: 'Feature',
+      //     properties: {},
+      //     geometry: {
+      //       type: 'LineString',
+      //       coordinates: [
+      //         [-122.483696, 37.833818],
+      //         [-122.483482, 37.833174],
+      //         [-122.483396, 37.8327],
+      //         [-122.483568, 37.832056],
+      //         [-122.48404, 37.831141],
+      //         [-122.48404, 37.830497],
+      //         [-122.483482, 37.82992],
+      //         [-122.483568, 37.829548],
+      //         [-122.48507, 37.829446],
+      //         [-122.4861, 37.828802],
+      //         [-122.486958, 37.82931],
+      //         [-122.487001, 37.830802],
+      //         [-122.487516, 37.831683],
+      //         [-122.488031, 37.832158],
+      //         [-122.488889, 37.832971],
+      //         [-122.489876, 37.832632],
+      //         [-122.490434, 37.832937],
+      //         [-122.49125, 37.832429],
+      //         [-122.491636, 37.832564],
+      //         [-122.492237, 37.833378],
+      //         [-122.493782, 37.833683],
+      //       ],
+      //     },
+      //   },
+      // });
+      // mapRef.current.addLayer({
+      //   id: 'route',
+      //   type: 'line',
+      //   source: 'route',
+      //   layout: {
+      //     'line-join': 'round',
+      //     'line-cap': 'round',
+      //   },
+      //   paint: {
+      //     'line-color': '#888',
+      //     'line-width': 8,
+      //   },
+      // });
+    });
+  }, []);
+
   return (
-    <div className='w-full h-full border rounded-xl flex items-center justify-center bg-white'>
-      <p className='text-xl font-bold'>Put map here</p>
-    </div>
+    <>
+      {/* <SearchBox
+        accessToken={import.meta.env.VITE_API_KEY}
+        map={mapRef.current!}
+        mapboxgl={mapboxgl}
+        value={inputValue}
+        onChange={(d) => {
+          setInputValue(d);
+        }}
+        marker
+      /> */}
+      <div
+        style={{ height: 'calc(100% - 20px)' }}
+        ref={mapContainerRef}
+        className='map-container'
+      ></div>
+    </>
   );
 };
